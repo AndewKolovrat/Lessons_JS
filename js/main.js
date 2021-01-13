74a4bedfa90e71494d14abc43a6dad46c8832fce
AndewKolovrat <andrewkolovrat@yandex.ru>
2021-01-13 18:55:33 +0300

    Complete

--- js/main.js
+++ js/main.js
@@ -1,26 +1,29 @@
-window.addEventListener('load', () => {
-    const settings = new Settings();
-    const status = new Status();
-    const board = new Board();
-    const snake = new Snake();
-    const food = new Food();
-    const menu = new Menu();
-    const game = new Game();
+const products = [
+    { id: 1, title: 'Notebook', price: 2000 },
+    { id: 2, title: 'Keyboard', price: 200 },
+    { id: 3, title: 'Mouse', price: 100 },
+    { id: 4, title: 'Gamepad', price: 87 },
+];
 
-    settings.init({ speed: 5, winLength: 5 });
-    board.init(settings, snake);
-    food.init(settings, snake, board);
-    game.init(settings, status, board, snake, menu, food);
+// так-как знаем структуру аргумента,
+// можно обобщить аргументы функции.
+const renderProduct = (item) => {
+    return `<li class="products_item">
+                <h3>${item.title}</h3>
+                <p>${item.price}</p>
+                <button class="products_item_buy-btn">Buy</button>
+            </li>`;
+};
+// хоть смысла в этом и нет, но задаем параметр по умолчанию :)
+const render = (productsList = products) => {
+    // один раз найдем элемент списка, что бы не дергать
+    // querySelector каждый проход цикла
+    let productListElem = document.querySelector('.products');
+    // прибавляем каждый проход цикла к соду элемента,
+    // код генерируемый функцией renderProduct
+    // тем самым избавляемся от дефолтного toString массива,
+    // а следовательно и запятых.
+    productsList.map(item => productListElem.innerHTML += renderProduct(item));
+};
 
-    board.renderBoard();
-    // render snake
-    board.renderSnake();
-    // render food
-    food.setNewFood();
-    // render walls
-    board.setNewWalls();
-
-    // start game
-    game.run();
-
-})
+render(products);
