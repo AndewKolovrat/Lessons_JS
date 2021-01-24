ce8c5bf8db75b936dddd4c7e9ad0c9709587f65d
AndewKolovrat <andrewkolovrat@yandex.ru>
2021-01-25 02:35:18 +0300

    1 task builded

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
+const txt = `One: 'Hi Mary.' 
+    Two: 'Oh, hi.'
+    One: 'How are you doing?'
+    Two: 'I'm doing alright.How about you ?'
+    One: 'Not too bad. The weather is great isn't it ?'
+    Two: 'Yes. It's absolutely beautiful today.'
+    One: 'I wish it was like this more frequently.'
+    Two: 'Me too.'
+    One: 'So where are you going now?'
+    Two: 'I'm going to meet a friend of mine at the department store.'
+    One: 'Going to do a little shopping?'
+    Two: 'Yeah, I have to buy some presents for my parents.'
+    One: 'What's the occasion ?'
+    Two: 'It's their anniversary.'
+    One: 'That's great.Well, you better get going.You don't want to be late.'
+    Two: 'I'll see you next time.'
+    One: 'Sure. Bye.'`;
 
 window.onload = () => {
     const list = new ProductsList(new Cart());
     // хром (CORS) ругается на запросы к файловой системе :(
     //list.getJson('./getProducts.json').then(data => list.handleData(data));
+
+    // ищем все кавычки перед которыми не граница слова
+    console.log(txt.replace(/\B\'/gm, '"'));
+
 };
