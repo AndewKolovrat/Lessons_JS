cf76ded02f45fd866d9926e521c82dc42da363f1
AndewKolovrat <andrewkolovrat@yandex.ru>
2021-01-25 03:59:59 +0300

    all ask finished

--- js/main.js
+++ js/main.js
@@ -24,4 +24,50 @@ window.onload = () => {
     // ищем все кавычки перед которыми не граница слова
     console.log(txt.replace(/\B\'/gm, '"'));
 
+    const form = document.querySelector('.footer_feedback'),
+        name = form.querySelector('.footer_feedback_name'),
+        phone = form.querySelector('.footer_feedback_phone'),
+        mail = form.querySelector('.footer_feedback_email'),
+        msg = form.querySelector('.footer_feedback_msg'),
+        wrg = form.querySelector('.footer_feedback_warning');
+
+    msg.value = txt.replace(/\B\'/gm, '"');
+
+    form.querySelector('.footer_feedback_btn').addEventListener('click', event => {
+        // все латинские буквы, цифры и знак подчеркивания
+        let re = /^[\w]+$/i;
+        if (!re.test(name.value)) {
+            wrg.textContent = `Warning! Wrong name field!`;
+            name.classList.add('warning');
+        }
+        else {
+            name.classList.remove('warning');
+            wrg.textContent = '';
+        }
+
+        // +цифра(3 цифры)3цифры-4цифры
+        re = /^\+\d\([\d]{3}\)[\d]{3}\-[\d]{4}$/i;
+        if (!re.test(phone.value)) {
+            wrg.textContent = `Warning! Wrong phone field!`;
+            phone.classList.add('warning');
+        }
+        else {
+            phone.classList.remove('warning');
+            wrg.textContent = '';
+        }
+
+        // (1 символ) (от 1 символа, точки или тирэ)
+        // ( @ ) ( от 1 символа ) ( точка ) ( от 2х до 4х символов )
+        re = /^[\w]{1}[\w\.\-]+@[\w]+\.[a-z]{2,4}$/i;
+        if (!re.test(mail.value)) {
+            wrg.textContent = `Warning! Wrong mail field!`;
+            mail.classList.add('warning');
+        }
+        else {
+            mail.classList.remove('warning');
+            wrg.textContent = '';
+        }
+
+    });
+
 };
