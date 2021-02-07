cc6461ee6d6c5cdb11214cf34a167e0eb9b8bf06
AndewKolovrat <andrewkolovrat@yandex.ru>
2021-02-07 12:25:04 +0300

    complete

--- js/main.js
+++ js/main.js
@@ -92,17 +92,118 @@ window.addEventListener('load', function OnWindowLoaded() {
 });
 
 ///////////////////////////// SPECS ////////////////////////////////////
-describe('Функция _division', () => {
+describe('Функция division', () => {
     it('При аргументах 12 и 3 должно возвращаться 4', () => {
         expect(division(12, 3)).toBe(4);
     });
     it('должна возвращать null при аргументах (null, 2)', () => {
         expect(division(null, 2)).toBeNull();
     });
-    it('должна возвращать null при аргументах (null, 2)', () => {
+    it('должна возвращать null при аргументах (2, null)', () => {
         expect(division(2, null)).toBeNull();
     });
-    it('должна возвращать null при аргументах (null, 2)', () => {
+    it('должна возвращать null при аргументах (null, null)', () => {
         expect(division(null, null)).toBeNull();
+    });
+    //
+    it('должна возвращать null при аргументах (undefined, 2)', () => {
+        expect(division(undefined, 2)).toBeNull();
+    });
+    it('должна возвращать null при аргументах (2, undefined)', () => {
+        expect(division(2, undefined)).toBeNull();
+    });
+    //
+    it('должна возвращать null при аргументах ("string", 2)', () => {
+        expect(division("string", 2)).toBeNull();
+    });
+    it('должна возвращать null при аргументах (2, "string")', () => {
+        expect(division(2, "string")).toBeNull();
+    });
+});
+
+describe('Функция addition', () => {
+    it('При аргументах 12 и 3 должно возвращаться 15', () => {
+        expect(addition(12, 3)).toBe(15);
+    });
+    it('должна возвращать null при аргументах (null, 2)', () => {
+        expect(addition(null, 2)).toBeNull();
+    });
+    it('должна возвращать null при аргументах (2, null)', () => {
+        expect(addition(2, null)).toBeNull();
+    });
+    it('должна возвращать null при аргументах (null, null)', () => {
+        expect(addition(null, null)).toBeNull();
+    });
+    //
+    it('должна возвращать null при аргументах (undefined, 2)', () => {
+        expect(addition(undefined, 2)).toBeNull();
+    });
+    it('должна возвращать null при аргументах (2, undefined)', () => {
+        expect(addition(2, undefined)).toBeNull();
+    });
+    //
+    it('должна возвращать null при аргументах ("string", 2)', () => {
+        expect(addition("string", 2)).toBeNull();
+    });
+    it('должна возвращать null при аргументах (2, "string")', () => {
+        expect(addition(2, "string")).toBeNull();
+    });
+});
+
+describe('Функция subtraction', () => {
+    it('При аргументах 12 и 3 должно возвращаться 9', () => {
+        expect(subtraction(12, 3)).toBe(9);
+    });
+    it('должна возвращать null при аргументах (null, 2)', () => {
+        expect(subtraction(null, 2)).toBeNull();
+    });
+    it('должна возвращать null при аргументах (2, null)', () => {
+        expect(subtraction(2, null)).toBeNull();
+    });
+    it('должна возвращать null при аргументах (null, null)', () => {
+        expect(subtraction(null, null)).toBeNull();
+    });
+    //
+    it('должна возвращать null при аргументах (undefined, 2)', () => {
+        expect(subtraction(undefined, 2)).toBeNull();
+    });
+    it('должна возвращать null при аргументах (2, undefined)', () => {
+        expect(subtraction(2, undefined)).toBeNull();
+    });
+    //
+    it('должна возвращать null при аргументах ("string", 2)', () => {
+        expect(subtraction("string", 2)).toBeNull();
+    });
+    it('должна возвращать null при аргументах (2, "string")', () => {
+        expect(subtraction(2, "string")).toBeNull();
+    });
+});
+
+describe('Функция multiplying', () => {
+    it('При аргументах 12 и 3 должно возвращаться 36', () => {
+        expect(multiplying(12, 3)).toBe(36);
+    });
+    it('должна возвращать null при аргументах (null, 2)', () => {
+        expect(multiplying(null, 2)).toBeNull();
+    });
+    it('должна возвращать null при аргументах (2, null)', () => {
+        expect(multiplying(2, null)).toBeNull();
+    });
+    it('должна возвращать null при аргументах (null, null)', () => {
+        expect(multiplying(null, null)).toBeNull();
+    });
+    //
+    it('должна возвращать null при аргументах (undefined, 2)', () => {
+        expect(multiplying(undefined, 2)).toBeNull();
+    });
+    it('должна возвращать null при аргументах (2, undefined)', () => {
+        expect(multiplying(2, undefined)).toBeNull();
+    });
+    //
+    it('должна возвращать null при аргументах ("string", 2)', () => {
+        expect(multiplying("string", 2)).toBeNull();
+    });
+    it('должна возвращать null при аргументах (2, "string")', () => {
+        expect(multiplying(2, "string")).toBeNull();
     });
 });
--- js/math.js
+++ js/math.js
@@ -1,15 +1,47 @@
 export const division = (arg, arg2) => {
-    return arg / arg2;
+    const param = checkArg(arg, arg2);
+    if (!param) {
+        return null;
+    }
+    return param.x / param.y;
 };
 export const addition = (arg, arg2) => {
-    return arg + arg2;
+    const param = checkArg(arg, arg2);
+    if (!param) {
+        return null;
+    }
+    return param.x + param.y;
 };
 export const subtraction = (arg, arg2) => {
-    return +arg - +arg2;
+    const param = checkArg(arg, arg2);
+    if (!param) {
+        return null;
+    }
+    return param.x - param.y;
 };
 export const multiplying = (arg, arg2) => {
-    return +arg * +arg2;
+    const param = checkArg(arg, arg2);
+    if (!param) {
+        return null;
+    }
+    return param.x * param.y;
 };
+
+const checkArg = (arg, arg2) => {
+    if (!arg || !arg2) {
+        return null;
+    }
+    const param = {
+        'x': 0,
+        'y': 0
+    };
+    param.x = Number.parseInt(arg);
+    param.y = Number.parseInt(arg2);
+    if (Number.isNaN(param.x) || Number.isNaN(param.y)) {
+        return null;
+    }
+    return param;
+}
 
 // module.exports = {
 //     division: division,
